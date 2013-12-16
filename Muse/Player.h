e171485d45edee4ec2dbb29efd7dbc7b6f0cf0de
zhu_peijun <peijun.zhu@drecom.co.jp>
2013-12-16 23:27:59 +0900

    fix a bug of play

---
+++ Muse/Player.h
@@ -0,0 +1,20 @@
+//
+//  Player.h
+//  Muse
+//
+//  Created by zhu peijun on 2013/12/16.
+//  Copyright (c) 2013年 zhu peijun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MediaPlayer/MediaPlayer.h>
+
+#import "XiamiObject.h"
+
+@interface Player : NSObject
+
++ (MPMoviePlayerController *) getMoviePlayer;
++ (XiamiObject *) getCurrentMusic;
++ (void) setCurrentMusic: (XiamiObject *) music;
+
+@end
