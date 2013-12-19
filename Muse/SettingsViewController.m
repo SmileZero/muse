fcd6cb9464b1ac3d4733fa0c8ae4dd6fe4392244
yan_runchen <runchen.yan@drecom.co.jp>
2013-12-19 09:47:27 +0900

    merge sound recognize

--- Muse/SettingsViewController.m
+++ Muse/SettingsViewController.m
@@ -174,6 +174,23 @@
     
     _menuItems = @[@"user",@"recognize"];
     _animating = NO;
+    
+    NSArray *familyNames = [[NSArray alloc] initWithArray:[UIFont familyNames]];
+    NSArray *fontNames;
+    NSInteger indFamily, indFont;
+    for (indFamily=0; indFamily<[familyNames count]; ++indFamily)
+    {
+        NSLog(@"Family name: %@", [familyNames objectAtIndex:indFamily]);
+        fontNames = [[NSArray alloc] initWithArray:
+                     [UIFont fontNamesForFamilyName:
+                      [familyNames objectAtIndex:indFamily]]];
+        for (indFont=0; indFont<[fontNames count]; ++indFont)
+        {
+            NSLog(@"    Font name: %@", [fontNames objectAtIndex:indFont]);
+        }
+        //[fontNames release];
+    }
+    //[familyNames release];
 }
 
 - (void)didReceiveMemoryWarning
